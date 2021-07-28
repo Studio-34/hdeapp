f6392e403bcfed29aa4fa07cb7e3b64813c0f326
Reece Walter <rwalter@S34-Web-Dev-iMac.local>
2021-07-28 09:25:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Pitch from './Comps/Pitch';
+import Keys from './Comps/Keys';
+import Card from './Comps/Card';
+import Header from './Comps/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Header />
+      <Pitch />
+      <Keys />
+      <h1 className="text-center mt-5">Pricing</h1>
+      <div className="row row-cols-1 row-cols-md-3 mt-5 mb-3 text-center">
+        <div className="col">
+          <Card cardTitle="Free"
+            cardText="This subscription level is designed to give the users the opportunity to test the functionality and features of the app without any payment obligations. A standard footer graphic is provided."
+            signUp="Sign Up"
+            price="$0"
+          />
+        </div>
+        <div className="col">
+          <Card cardTitle="Premium"
+            cardText="This subscription level is designed to give the users the opportunity to test the functionality and features of the app without any payment obligations. A standard footer graphic is provided."
+            signUp="Sign Up"
+            price="$4.99"
+          />
+        </div>
+        <div className="col">
+          <Card cardTitle="Enterprise"
+            cardText="This subscription level is designed to give the users the opportunity to test the functionality and features of the app without any payment obligations. A standard footer graphic is provided."
+            signUp="Sign Up"
+            price="$59.99"
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Comps/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="text-center my-5">
+            <h1>High Defention Events</h1>
+            <h2>The HDE App</h2>
+        </div>
+    )
+}
